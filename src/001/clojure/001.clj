203cf90534f11192fa76b19eb41bbc1108cd8f29
Marco Valtas <mavcunha@gmail.com>
2021-02-08 17:08:47 -0500

    First solution in clojure

--- src/001/clojure/001.clj
+++ src/001/clojure/001.clj
@@ -1,2 +1,6 @@
-(require '[clojure.pprint])
-(clojure.pprint/pprint (range 1 10))
+(def target 999)
+(defn- sumDivisibleBy [n]
+  (let [p (Math/floor (/ target n))]
+    (/ (* n (* p (+ 1 p))) 2) ))
+
+(print (- (+ (sumDivisibleBy 3) (sumDivisibleBy 5)) (sumDivisibleBy 15)))
