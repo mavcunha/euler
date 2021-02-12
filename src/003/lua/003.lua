9adf5798bacec0bcab8303cc1fe64ebd74f4a6d6
Marco Valtas <mavcunha@gmail.com>
2021-02-11 20:09:13 -0500

    Solution for 003 in Lua

--- src/003/lua/003.lua
+++ src/003/lua/003.lua
@@ -1,21 +1,34 @@
-local n = 100
+local n = 600851475143
+local f = math.floor(math.sqrt(n))
 local A = {}
 
-for i = 2, n do
-  A[i] = true
-end
-
-for i=2, math.floor(math.sqrt(n)) do
-  if A[i] then
-    for j = math.pow(i, 2), n, i do
-      A[j] = false
+-- sieve - the table is fliped so we
+-- do not need to initialize it
+for i=2, math.floor(math.sqrt(f)) do
+  if not A[i] then
+    for j = math.pow(i, 2), f, i do
+      A[j] = true
     end
   end
 end
 
+-- prime fator
+local F, t = {}, n
 for i = 2, #A do
-  if A[i] then
-    print(i)
+  if not A[i] then
+    while t % i == 0 do
+      F[i] = true
+      t = t / i
+    end
   end
 end
 
+-- find the max prime factor
+local maxdiv = 0
+for k, _ in pairs(F) do
+  if k > maxdiv then
+    maxdiv = k
+  end
+end
+
+print(maxdiv)
